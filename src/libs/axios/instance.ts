1230af98b7166f98c116bef1d2b7bbfa6ed3c5ad
irpanzy <irfanoputra27@gmail.com>
2025-08-23 10:34:03 +0700

    feat(axios): add axios instance with request/response interceptors and environment configuration

---
+++ src/libs/axios/instance.ts
@@ -0,0 +1,32 @@
+import environment from "@/config/environment";
+import axios from "axios";
+
+const headers = {
+  "Content-Type": "application/json",
+};
+
+const instance = axios.create({
+  baseURL: environment.API_URL,
+  headers,
+  timeout: 60 * 1000,
+});
+
+instance.interceptors.request.use(
+  (request) => {
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
