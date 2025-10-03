ddc4000f4e0b5d8f4837b493e129458b494f527b
irpanzy <irfanoputra27@gmail.com>
2025-10-03 13:34:54 +0700

    feat(auth): add JWT-based credentials authentication with session extension

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,80 @@
+import environment from "@/config/environment";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { JWTExtended, SessionExtended, UserExtended } from "@/types/Auth";
+import authServices from "@/services/auth.service";
 
 export default NextAuth({
-  providers: [], // Tambahkan provider sesuai kebutuhan
+  session: {
+    strategy: "jwt",
+    maxAge: 60 * 60 * 24, // 1 day
+  },
+  secret: environment.AUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        identifier: { label: "identifier", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(
+        credentials: Record<"identifier" | "password", string> | undefined,
+      ): Promise<UserExtended | null> {
+        const { identifier, password } = credentials as {
+          identifier: string;
+          password: string;
+        };
+
+        const result = await authServices.login({
+          identifier,
+          password,
+        });
+
+        const accessToken = result.data.data;
+
+        const profileResult = await authServices.getMeProfile(accessToken);
+        const user = profileResult.data.data;
+
+        if (
+          accessToken &&
+          result.status === 200 &&
+          user._id &&
+          profileResult.status === 200
+        ) {
+          return { ...user, accessToken };
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({
+      token,
+      user,
+    }: {
+      token: JWTExtended;
+      user: UserExtended | null;
+    }) {
+      if (user) {
+        token.user = user as UserExtended;
+      }
+      return token;
+    },
+    async session({
+      session,
+      token,
+    }: {
+      session: SessionExtended;
+      token: JWTExtended;
+    }) {
+      if (token.user) {
+        session.user = token.user as UserExtended;
+        session.accessToken = token.user?.accessToken;
+      }
+      return session;
+    },
+  },
 });
